73f17e538656e489d32c762f90fb7d3c9ac5b4d5
Abosheetah <abosheetah@gmail.com>
2018-11-10 19:30:09 +0200

    initial project

---
+++ Models/Office/Career.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContractingCompany.Models.Office{
+    public class Career{
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CareerTypeID { get; set; }
+        [ForeignKey("CareerTypeID")]
+        public CareerType CareerType { get; set; }
+        public bool? isSysUser { get; set; }
+        public bool? isSysAdmin { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
+    }
+}
