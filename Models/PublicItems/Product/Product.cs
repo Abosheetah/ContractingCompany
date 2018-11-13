569cca3c7a33f2625c0d633cd6b9d38a08a6513e
= <=>
2018-11-13 09:41:35 +0200

    13/11/2018

---
+++ Models/PublicItems/Product/Product.cs
@@ -0,0 +1,8 @@
+namespace ContractingCompany.Models.PublicItems.Product{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
